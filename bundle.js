f3f84101dd1f4c62fc734f50c097458ecc0702f2
LucasLixo <paulolucasalves2006@gmail.com>
2024-12-14 20:29:39 -0300

    Add Version Minify

--- bundle.js
+++ bundle.js
@@ -2,7 +2,6 @@ const esbuild = require('esbuild');
 
 const entryFile = './build/wvp.js';
 const outputBundle = './dist/wvp.js';
-/* const outputMinified = './dist/wvp.min.js'; */
 
 esbuild.build({
     entryPoints: [entryFile],
@@ -15,16 +14,3 @@ esbuild.build({
     sourcemap: false,
     logLevel: 'info',
 })
-/* esbuild.build({
-    entryPoints: [entryFile],
-    outfile: outputMinified,
-    bundle: true,
-    minify: true,
-    platform: 'browser',
-    target: 'es5',
-    format: 'esm',
-    sourcemap: false,
-    logLevel: 'info',
-    minifyIdentifiers: false,
-    keepNames: true,
-}); */
